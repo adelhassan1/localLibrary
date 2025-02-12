56f81be268053c2aa4af5856bf732095887fd11d
adelhassan1 <tomasander12@gmail.com>
2025-02-13 00:25:39 +0200

    updated the authorController

--- controllers/api/authorController.js
+++ controllers/api/authorController.js
@@ -5,12 +5,30 @@ const { body, validationResult } = require('express-validator');
 
 //Display list of all authors.
 exports.author_list = asyncHandler(async (req, res, next) => {
-	res.json({message: "Not implemented yet"});
+	const allAuthors = await Author.find({}).sort({ family_name: 1 }).exec();
+
+	res.json({
+		Authors: allAuthors,
+	});
 });
 
 //Display detail page for a specifc author.
 exports.author_detail = asyncHandler(async (req, res, next) => {
-	res.json({message: "Not implemented"});
+	const [author, allBooksByAuthor] = await Promise.all([
+		Author.findById(req.params.id).exec(),
+		Book.find({ author: req.params.id }, "title summary").exec(),
+	]);
+
+	if (author === null) {
+		const err = new Error("Author Not Found");
+		err.status = 404;
+		return next(err);
+	}
+
+	res.json({
+		author: author,
+		author_books: allBooksByAuthor,
+	});
 });
 
 //Display Author create form on get.
