62a959729f8f3ee16b7fca0129b0b4af4f239f19
adelhassan1 <tomasander12@gmail.com>
2025-02-15 09:27:06 +0200

    fixed the cast to objectid error

--- routes/api/v1/author.js
+++ routes/api/v1/author.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 const author_controller = require('../../../controllers/api/authorController');
 
-router.get('/authors', author_controller.author_list);
-router.get('/author/:id', author_controller.author_detail);
 
 router.get('/author/create', author_controller.author_create_get);
 router.post('/author/create', author_controller.author_create_post);
@@ -14,5 +12,8 @@ router.post('/author/:id/delete', author_controller.author_detele_post);
 
 router.get('/author/:id/update', author_controller.author_update_get);
 router.post('/author/:id/update', author_controller.author_update_post);
+
+router.get('/authors', author_controller.author_list);
+router.get('/author/:id', author_controller.author_detail);
 
 module.exports = router;
