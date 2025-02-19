211664a22f1614a4f3e81f7eb0bcca263e81ffa4
adelhassan1 <tomasander12@gmail.com>
2025-02-20 01:33:06 +0200

    new home

---
+++ controllers/api/home.js
@@ -0,0 +1,22 @@
+const Book = require('../../models/book');
+const Author = require('../../models/author');
+const Genre = require('../../models/genre');
+const asyncHandler = require('express-async-handler');
+
+exports.index = asyncHandler(async (req, res, next) => {
+	const [
+		numBooks,
+		numAuthors,
+		numGenres,
+	] = await Promise.all([
+		Book.countDocuments({}).exec(),
+		Author.countDocuments({}).exec(),
+		Genre.countDocuments({}).exec(),
+	]);
+
+	res.json({
+		Books: numBooks,
+		Authors: numAuthors,
+		Genres: numGenres,
+	});
+});
--- routes/api/v1/book.js
+++ routes/api/v1/book.js
@@ -3,18 +3,15 @@ const router = express.Router();
 
 const book_controller = require('../../../controllers/api/bookController');
 
-router.get('/', book_controller.index);
+router.get('/', book_controller.book_list);
 
-router.get('/book/create', book_controller.book_create_get);
-router.post('/book/create', book_controller.book_create_post);
+router.get('/:id', book_controller.book_detail);
 
-router.get('/book/:id/delete', book_controller.book_delete_get);
-router.post('/book/:id/delete', book_controller.book_delete_post);
+router.post('/create', book_controller.book_create);
 
-router.get('/book/:id/update', book_controller.book_update_get);
-router.post('/book/:id/update', book_controller.book_update_post);
+router.delete('/:id', book_controller.book_delete);
 
-router.get('/books', book_controller.book_list);
-router.get('/book/:id', book_controller.book_detail);
+router.patch('/:id', book_controller.book_update);
+
 
 module.exports = router;
